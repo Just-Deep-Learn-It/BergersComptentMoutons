c3ef558cf3cac620a5adee2a2df63581d9d5db52
Just-Deep-Learn-It <48248579+Just-Deep-Learn-It@users.noreply.github.com>
2019-03-18 22:08:42 +0100

    Add files via upload

---
+++ loaders/shangaitech_loader.py
@@ -0,0 +1,92 @@
+import os
+import os.path
+import numpy as np
+import torch 
+import torch.utils.data as data 
+import torchvision
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+class ShanghaiTechLoader(torch.utils.data.Dataset):
+
+    def __init__(self, path_to_data=data_path, mode='TRAIN', transform=None, target_transform=None):
+        
+        self.transform = transform
+        self.target_transform = target_transform
+        self.mode = mode  # training, validation or test set
+        
+
+        if self.mode == 'TRAIN':
+            data_dir = os.path.join(path_to_data, 'train_data/')
+            count_path=os.path.join(data_dir,'count_train.npy')
+        elif self.mode == 'TEST':
+            data_dir = os.path.join(path_to_data, 'test_data/')
+            count_path=os.path.join(data_dir,'count_test.npy')
+        elif self.mode == 'VAL':
+            data_dir = os.path.join(path_to_data, 'validation_data/')
+            count_path=os.path.join(data_dir,'count_validation.npy')
+        
+        images_dir=os.listdir(os.path.join(data_dir,'images/'))
+        N=len(images_dir)
+        self.counts=[]
+        self.data=[]
+        self.targets=[]
+        
+        count=np.load(count_path)
+        idx=0
+        for image_name in images_dir:
+            image_path=os.path.join(data_dir,'images_cropped/',image_name)
+            x = image_name.split('.jpg',1)[0].split('_')
+            i,num=x[-1], x[-2] # dertmining original image index and numvber of the crop , works only for train data
+            density_name='GT_density_'+image_name.split('.jpg',1)[0]+'.npy'
+            density_path=os.path.join(data_dir,'GT_density_map_cropped/',density_name)
+            #print(os.path.join(data_dir,'images/'))
+            image=plt.imread(image_path)
+            image=np.reshape(image,(3,224,224))
+            self.data.append(image)
+
+            density_map = np.load(density_path)
+
+            self.counts.append(count[i+9*(num-1)])
+            self.targets.append(density_map)
+            
+            idx+=1
+            if idx>5:
+                break
+                
+        self.data, self.targets= np.asarray(self.data, dtype=float) , np.asarray(self.targets, dtype=float)
+        self.counts=np.asarray(self.counts, dtype=float)
+        self.read_lists()
+        self.transforms = self.get_transforms(custom_transforms)
+
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is the density map.
+        """
+        img= self.data[index] 
+        target=[self.counts[index],self.targets[index]]
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+
+    def __len__(self):
+        return len(self.data)
+
+    def labels(self):
+        return self.targets
+    
+    def data(self):
+        return self.data
---
+++ toolbox/metrics.py
@@ -0,0 +1,159 @@
+
+'''
+Code snippets for keeping track of evaluation metrics
+'''
+
+import numpy as np
+import json
+
+
+'''
+                                .
+                              .o8
+ooo. .oo.  .oo.    .ooooo.  .o888oo  .ooooo.  oooo d8b  .oooo.o
+`888P"Y88bP"Y88b  d88' `88b   888   d88' `88b `888""8P d88(  "8
+ 888   888   888  888ooo888   888   888ooo888  888     `"Y88b.
+ 888   888   888  888    .o   888 . 888    .o  888     o.  )88b
+o888o o888o o888o `Y8bod8P'   "888" `Y8bod8P' d888b    8""888P'
+'''
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def value(self):
+        return self.avg
+
+class SumMeter(object):
+    """Computes and stores the sum and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+
+    def value(self):
+        return self.sum
+
+
+class ValueMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+
+    def update(self, val):
+        self.val = val
+
+    def value(self):
+        return self.val
+
+
+
+
+
+def make_meters(num_classes=2):
+    meters_dict = {
+        'loss': AverageMeter(),
+        'squared_mse': AverageMeter(),
+        'mae':AverageMeter()
+        'acc_class': ValueMeter(),
+        'fwavacc': ValueMeter(),
+        'batch_time': AverageMeter(),
+        'data_time': AverageMeter(),
+        'epoch_time': SumMeter(),
+    }
+    return meters_dict
+
+
+
+def save_meters(meters, fn, epoch=0):
+
+    logged = {}
+    for name, meter in meters.items():
+        logged[name] = meter.value()
+
+    if epoch > 0:
+        logged['epoch'] = epoch
+
+    print(f'Saving meters to {fn}')
+    with open(fn, 'w') as f:
+        json.dump(logged, f)
+
+
+'''
+ .oooo.o  .ooooo.   .ooooo.  oooo d8b  .ooooo.   .oooo.o
+d88(  "8 d88' `"Y8 d88' `88b `888""8P d88' `88b d88(  "8
+`"Y88b.  888       888   888  888     888ooo888 `"Y88b.
+o.  )88b 888   .o8 888   888  888     888    .o o.  )88b
+8""888P' `Y8bod8P' `Y8bod8P' d888b    `Y8bod8P' 8""888P'
+'''
+
+def evaluate(hist):
+    acc = np.diag(hist).sum() / hist.sum()
+    acc_cls = np.diag(hist) / hist.sum(axis=1)
+    acc_cls = np.nanmean(acc_cls)
+    iu = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)+1e-10)
+    mean_iu = np.nanmean(iu)
+    freq = hist.sum(axis=1) / (hist.sum()+ 1e-10)
+    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+    return acc, acc_cls, mean_iu, fwavacc
+
+
+def accuracy_classif(output, target, topk=1):
+    """Computes the precision@k for the specified values of k"""
+    maxk = 1
+    # maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    # res = []
+    correct_k = correct[:maxk].view(-1).float().sum(0)
+    correct_k.mul_(1.0 / batch_size)
+    res = correct_k.clone()
+
+    return res.item(), pred, target
+
+
+def accuracy_regression(output, target):
+    mae_mean = (output - target).abs().mean()
+    mse_mean = (output - target).pow(2).mean()
+    rmse = (output - target).pow(2).mean().sqrt()
+    return mae_mean.item(), mse_mean.item(), rmse_mean.item()
+
+
+def fast_hist(pred, label, n):
+    k = (label >= 0) & (label < n)
+    return np.bincount(
+        n * label[k].astype(int) + pred[k], minlength=n ** 2).reshape(n, n)
+
+
+def per_class_iu(hist):
+    return np.diag(hist) / (hist.sum(1) + hist.sum(0) - np.diag(hist)+ 1e-10)
+
+
